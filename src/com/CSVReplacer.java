4034c91ac8933263a131b33a117e9fe81225f0dc
Daniele Albanese <d.albanese1@studenti.unimol.it>
2019-12-12 22:47:04 +0100

    Parser funzionante con FusionBuildString da modificare i placeholder delle immagini

--- src/com/CSVReplacer.java
+++ src/com/CSVReplacer.java
@@ -1,6 +1,6 @@
 package com;
 
-import com.attractors.AgenzieViaggiETurismo;
+import com.attractors.StruttureRicettive;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -10,7 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class CSVReplacer {
-	private static String nameOfCsv = "agenzieViaggiETurismo";
+	private static String nameOfCsv = "struttureRicettive";
 
 	private static final String csvPath = "/home/djd/Documents/Turismo Molise/" + nameOfCsv + ".csv";
 	private static final String newCsvPath = "/home/djd/Documents/Turismo Molise/" + nameOfCsv + "_nuovo.csv";
@@ -29,12 +29,12 @@ public class CSVReplacer {
 						CSVWriter.DEFAULT_LINE_END
 				)) {
 
-			CsvToBean<AgenzieViaggiETurismo> csvToBean = new CsvToBeanBuilder(reader)
-					.withType(AgenzieViaggiETurismo.class)
+			CsvToBean<StruttureRicettive> csvToBean = new CsvToBeanBuilder(reader)
+					.withType(StruttureRicettive.class)
 					.withIgnoreLeadingWhiteSpace(true)
 					.build();
 
-			for (AgenzieViaggiETurismo agenzieViaggiETurismo : csvToBean) {
+			for (StruttureRicettive atr : csvToBean) {
 				String stringToReplace = fusionBuildString.getContent();
 
 				stringToReplace = stringToReplace.replaceAll("\n", "");
@@ -43,15 +43,24 @@ public class CSVReplacer {
 				stringToReplace = stringToReplace.replaceAll(regex, "]\n\n[");
 
 				for (Fields fields : Fields.values()) {
-					if (!agenzieViaggiETurismo.getField(fields).equals("-")) {
-						if(fields.name().contains("CAMPO_TELEFONO") || fields.name().contains("CAMPO_INDIRIZZO")) {
-							stringToReplace = stringToReplace.replaceAll(fields.name(), agenzieViaggiETurismo.getField(fields) + "\n");
+					if (!atr.getField(fields).equals("-")) {
+						if (fields.name().contains("CAMPO_TELEFONO") || fields.name().contains("CAMPO_INDIRIZZO")) {
+							stringToReplace = stringToReplace.replaceAll(fields.name(), atr.getField(fields) + "\n");
+						} else if (fields.name().contains("CAMPO_TIPOLOGIA")) {
+							stringToReplace = stringToReplace.replace("CAMPO_TIPOLOGIA", atr.getField(fields));
+
+							if (!atr.getField(Fields.CAMPO_ALTRA_TIPOLOGIA).equals("-")) {
+								stringToReplace = stringToReplace.replace("CAMPO_ALTRA_TIPOLOGIA", atr.getField(Fields.CAMPO_ALTRA_TIPOLOGIA));
+							} else {
+								stringToReplace = stringToReplace.replace("|", "");
+								stringToReplace = stringToReplace.replace("CAMPO_ALTRA_TIPOLOGIA", "");
+							}
 						} else {
-							stringToReplace = stringToReplace.replaceAll(fields.name(), agenzieViaggiETurismo.getField(fields));
+							stringToReplace = stringToReplace.replaceAll(fields.name(), atr.getField(fields));
 						}
 					}
 
-					if (agenzieViaggiETurismo.getField(fields).equals(fields.name()) || agenzieViaggiETurismo.getField(fields).equals("-")) {
+					if (atr.getField(fields).equals(fields.name()) || atr.getField(fields).equals("-")) {
 						BufferedReader bufferedReader = new BufferedReader(new StringReader(stringToReplace));
 						String line;
 
